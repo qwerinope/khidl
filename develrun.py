9b98ac077da94a1870afa45ce85e30e9dc53c452
qweri0p <71005650+qweri0p@users.noreply.github.com>
2024-11-28 09:59:39 +0100

    v1.2.0

--- develrun.py
+++ develrun.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # This is for development only
+import sys, re
 from khidl.app import CLI
 if __name__=="__main__":
-    CLI()
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.exit(CLI())
